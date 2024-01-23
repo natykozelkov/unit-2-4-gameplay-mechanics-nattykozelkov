cedc1708379f9d44358e6d505eecd460c7d852d0
natykozelkov <nkozelkova25@sandomenico.org>
2024-01-23 13:56:47 -0800

    2.4.3 Complete and push to GitHub

--- Assets/Scripts/Other Controls/MoveToTarget.cs
+++ Assets/Scripts/Other Controls/MoveToTarget.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
  * MoveToTarget is the component of the Ice Sphere.
  * 
  * Naty Kozelkova
- * January 22, 2024 Version 1.0
+ * January 23, 2024 Version 1.0
  *******************************************/
 
 
@@ -16,22 +16,24 @@ public class MoveToTarget : MonoBehaviour
     [SerializeField] private NavMeshAgent navMeshAgent;
 
     private GameObject target;
-    private Rigidbody targetRB;
+    private Rigidbody targetRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player");
+        targetRb = target.GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsTarget();
     }
 
     void MoveTowardsTarget()
     {
-
+        navMeshAgent.SetDestination(targetRb.transform.position);
     }
 }
